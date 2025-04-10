7a308721266d751823a0c22de49e53d78d3c3735
andria21 <andriamarqarovi33@gmail.com>
2025-04-10 22:05:05 +0400

    fixed logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Logoo from '../../cutdm.png';
+
 export function Logo() {
   return (
     <img 
-      src="../../cutdm.png"
+      src={Logoo}
       alt="DM's AI - Business Automation Solutions"
       className="w-8 h-8"
       loading="eager"
